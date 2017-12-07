1528f54703574854a8972eaee5b2af468cb86902
Ben <bzhao18@github.com>
2017-12-07 14:25:19 -0500

    changes

--- morse.py
+++ morse.py
@@ -2,34 +2,18 @@ class morse:
 	def __init__(self, message, location):
 		self.message = message
 		self.location = location
-        self.cipher = ''
-        CODE = {'A': '.-',     'B': '-...',   'C': '-.-.', 
-        'D': '-..',    'E': '.',      'F': '..-.',
-        'G': '--.',    'H': '....',   'I': '..',
-        'J': '.---',   'K': '-.-',    'L': '.-..',
-        'M': '--',     'N': '-.',     'O': '---',
-        'P': '.--.',   'Q': '--.-',   'R': '.-.',
-        'S': '...',    'T': '-',      'U': '..-',
-        'V': '...-',   'W': '.--',    'X': '-..-',
-        'Y': '-.--',   'Z': '--..',
-        
-        '0': '-----',  '1': '.----',  '2': '..---',
-        '3': '...--',  '4': '....-',  '5': '.....',
-        '6': '-....',  '7': '--...',  '8': '---..',
-        '9': '----.' 
-        }
-    def encrypt(self, message):
-        cipher = ''
-        for letter in message:
-            if letter != ' ':
-                cipher += CODE[letter] + ''
-            else:
-                cipher += ''
-        return cipher
-        
-       
-       
+		self.translation = ''
+		self.alpha = 'abcdefghijklmnopqrstuvwxyz'
+		self.code = [ '.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
+		for x in range(len(self.message)):
+			if self.message[x].isalpha():
+				self.alpha[x] = self.code[x]
+				return self.translation.append(self.code)
+			else: 
+				self.code[x] = self.message[x]
+				self.translation = self.translation.append(self.code)
 
+       
 
 
 
