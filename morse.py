b57bbfe8764004655453497a6cde67f633f2fc45
Ben <bzhao18@github.com>
2017-12-06 23:14:22 -0500

    morse code

---
+++ morse.py
@@ -0,0 +1,35 @@
+class morse:
+	def __init__(self, message, location):
+		self.message = message
+		self.location = location
+        self.cipher = ''
+        CODE = {'A': '.-',     'B': '-...',   'C': '-.-.', 
+        'D': '-..',    'E': '.',      'F': '..-.',
+        'G': '--.',    'H': '....',   'I': '..',
+        'J': '.---',   'K': '-.-',    'L': '.-..',
+        'M': '--',     'N': '-.',     'O': '---',
+        'P': '.--.',   'Q': '--.-',   'R': '.-.',
+        'S': '...',    'T': '-',      'U': '..-',
+        'V': '...-',   'W': '.--',    'X': '-..-',
+        'Y': '-.--',   'Z': '--..',
+        
+        '0': '-----',  '1': '.----',  '2': '..---',
+        '3': '...--',  '4': '....-',  '5': '.....',
+        '6': '-....',  '7': '--...',  '8': '---..',
+        '9': '----.' 
+        }
+    def encrypt(self, message):
+        cipher = ''
+        for letter in message:
+            if letter != ' ':
+                cipher += CODE[letter] + ''
+            else:
+                cipher += ''
+        return cipher
+        
+       
+       
+
+
+
+
